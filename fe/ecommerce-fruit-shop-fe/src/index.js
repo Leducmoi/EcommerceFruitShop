280f32e90ac739a32c3d54adf3139ed0f8a4b1b1
Le Duc Moi <leducmoi@dtu.edu.vn>
2025-06-05 15:48:09 +0700

    Tạo được các phần chung nhất của Cây thư mục

--- fe/ecommerce-fruit-shop-fe/src/index.js
+++ fe/ecommerce-fruit-shop-fe/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
+import HomePage from './Pages/Users/homePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Hello Moi</h1>
+  <HomePage />
 );
 
 reportWebVitals();
