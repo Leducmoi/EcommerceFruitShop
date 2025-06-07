db5e72b572306970053944085a1304c791732270
Le Duc Moi <leducmoi@dtu.edu.vn>
2025-06-07 23:39:57 +0700

    Done Header Bar

--- fe/ecommerce-fruit-shop-fe/src/Pages/Users/theme/header/index.js
+++ fe/ecommerce-fruit-shop-fe/src/Pages/Users/theme/header/index.js
@@ -1,9 +1,67 @@
 import { memo } from "react";
 import './style.scss';
+import { AiOutlineFacebook, AiOutlineTwitter, AiOutlineInstagram, AiOutlineLinkedin, AiOutlineUser, AiOutlineMail } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { formatCurrency } from "Utils/formater";
+
 const header = () => {
   return (
-    <div className="header__top">
-      <div className="container">Header</div>
+    <div className="header-top">
+      <div className="container">
+        <div className="row">
+
+
+          <div className="col-6 header-top__left">
+            <ul>
+              <li><AiOutlineMail /><p>Hello@gmail.com</p></li>
+
+              <li>
+                Miễn phí vận chuyển cho đơn hàng từ&nbsp;
+                <span className="price">{formatCurrency(500000)}</span>
+              </li>
+            </ul>
+          </div>
+
+
+
+
+          <div className="col-6 header-top__right">
+            <ul>
+              <li>
+                <Link to={""}>
+                  <AiOutlineFacebook />
+                </Link>
+
+              </li>
+              <li>
+                <Link to={""}>
+                  <AiOutlineTwitter />
+                </Link>
+
+              </li>
+              <li>
+                <Link to={""}>
+                  <AiOutlineInstagram />
+                </Link>
+
+              </li>
+              <li>
+                <Link to={""}>
+                  <AiOutlineLinkedin />
+                </Link>
+
+              </li>
+
+              <li>
+                <Link to={""}>
+                  <AiOutlineUser />
+                </Link>
+                <span>Đăng nhập</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
